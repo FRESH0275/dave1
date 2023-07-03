1eb92d3157824686a0a67bac4e77b287eaac2641
FRESH0275 <138433455+FRESH0275@users.noreply.github.com>
2023-07-03 13:02:50 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print("hello")
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from random import randint
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Развод лоха')
+but = QPushButton('Лох')
+text = QLabel('Нажми чтоб узнать лоха')
+winner = QLabel('?')
+line = QVBoxLayout()
+line.addWidget(text, alignment = Qt.AlignCenter)
+line.addWidget(winner, alignment = Qt.AlignCenter)
+line.addWidget(but, alignment = Qt.AlignCenter)
+main_win.setLayout(line)
+def show_winner():
+    number = randint(0, 99)
+    winner.setText(str(number))
+    text.setText('Лох:')
+
+
+but.clicked.connect(show_winner)
+main_win.show()
+app.exec_()
